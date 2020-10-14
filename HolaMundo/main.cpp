a7c19f2547ee5bb71a16565f026c7302e279bd8e
Bernardo Jose Martinez Vallecillo <bernardo.martinez@unitec.edu>
2020-10-14 10:26:22 -0600

    Actualizacion

---
+++ HolaMundo/main.cpp
@@ -0,0 +1,16 @@
+//importar la libreria para output and imput
+#include <iostream>
+#include <conio.h>
+
+int main() {
+
+	//console output
+	/*Este es un comentario
+	* en varias
+	* lineas*/
+
+	std::cout << "Bienvenidos a Prgramacion 3!\n";
+
+	_getch();
+	return 0;
+}
