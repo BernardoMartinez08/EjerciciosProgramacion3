5fa04d62160736b3b6aba40bbd89bb5be487a6d5
Bernardo Jose Martinez Vallecillo <bernardo.martinez@unitec.edu>
2020-10-15 11:24:03 -0600

    Update Digitos Enteros 2

--- DigitosEnteros/main.cpp
+++ DigitosEnteros/main.cpp
@@ -8,6 +8,7 @@ int main(){
 	cout << "Ingrese un numero de 5 digitos: ";
 	int numero = 0;
 	cin >> numero;
+	int numero2 = numero;
 
 	//Solucion 1
 	int digito1 = numero / 10000;
@@ -23,7 +24,7 @@ int main(){
 	int divisor = 10000;
 	int temp = numero;
 
-	cout << "Los digitos son: { ";
+	cout << "Los digitos son: {   ";
 
 	for (int i = 0; i < 5; i++) {
 		temp = numero / divisor;
@@ -35,6 +36,23 @@ int main(){
 			cout << "}\n";
 		}
 	}
+
+	//Solucion 3
+	int divisor2 = 10000;
+	int numeros[5];
+
+	cout << "Los digitos son: {   ";
+
+	for (int i = 0; i < 5; i++) {
+		numeros[i] = numero2 / divisor2;
+		numero2 = numero2 % divisor2;
+		divisor2 = divisor2 / 10;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		cout << numeros[i] << "   ";
+	}
+	cout << "}\n";
 
 	_getch();
 }
